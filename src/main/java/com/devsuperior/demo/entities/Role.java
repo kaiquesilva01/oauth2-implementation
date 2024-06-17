70742440825cb02693eadf0a5d820d18b248779b
Notebook <kaique.andruid@gmail.com>
2024-06-17 10:14:15 -0300

    User Role model

---
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -0,0 +1,57 @@
+package com.devsuperior.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String authority;
+
+    public Role() {}
+
+    public Role(Long id, String authority) {
+        Id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return authority != null ? authority.hashCode() : 0;
+    }
+}
