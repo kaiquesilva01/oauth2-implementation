908ae9d19eac002a8092683e01576c7c71bff98e
Notebook <kaique.andruid@gmail.com>
2024-06-17 12:10:18 -0300

    Spring Security Checklist(loadUserByUsername), not tested

--- src/main/java/com/devsuperior/demo/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/demo/repositories/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.devsuperior.demo.repositories;
 
+import com.devsuperior.demo.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.demo.entities.Product;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
